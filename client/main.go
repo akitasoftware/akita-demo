8a7b2589cf6a55c4051630beb10ab0bdd6479f21
Versilis Tyson <100976287+versilis@users.noreply.github.com>
2023-04-26 13:38:16 -0500

    Add Demo Client (#3)
    This create a new demo client application that is configured to send periodic requests to our custom WireMock demo server. Along with factoring out the demo functionality from https://github.com/akitasoftware/docker-extension, changes include:
    
        Sending analytics events during error handling
        Adding a new request for fetching users (which always returns a 200)
        Minor adjustments to mock ID generation to improve performance
    
    Depends on: #2
    
    Co-authored-by: Jed Liu <liujed@users.noreply.github.com>

---
+++ client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"akitasoftware.com/demo-client/datasource"
+	_ "embed"
+	"flag"
+	"log"
+	"net/http"
+)
+
+//go:embed application.yml
+var applicationYML []byte
+
+func main() {
+	flag.Parse()
+
+	config, err := ParseConfiguration(applicationYML)
+	if err != nil {
+		log.Fatalf("failed to parse config: %v", err)
+	}
+
+	server := datasource.NewDemoServer("http://akita-demo-server:8080", http.DefaultClient)
+
+	app := NewApp(config, server)
+
+	app.SendEvent("Demo Client Started", map[string]any{})
+
+	app.HandleDemoTasks()
+}
